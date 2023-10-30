7feed8f303fd76f771901c36034250694baf1cf0
Yoleny <yoleny.salme28@gmail.com>
2023-10-30 12:37:36 -0600

    Agregando clase SuperHeroe

--- EjercicioClase/Program.cs
+++ EjercicioClase/Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+public class SuperHeroe
+{
+    public string nombre { get; set; }
+    public string ientidadSecreta { get; set; }
+    public string ciudad { get; set; }
+    public  string puedeVolar { get; set; }
+    public string superPoder { get; set; }
+   
+
+
+ 
+}
